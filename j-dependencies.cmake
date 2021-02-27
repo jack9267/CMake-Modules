27916d03d0d0fc7c1bff540291af23a307317c58
Jack Powell <jack9267@users.noreply.github.com>
2021-02-27 12:15:15 +0000

    Fix some dep stuff

--- j-dependencies.cmake
+++ j-dependencies.cmake
@@ -1,16 +1,15 @@
 
-if(WIN32)
-	set(DEPENDENCIES_PATH "$ENV{jdependencies_home}" CACHE PATH "Dependencies location")
+set(DEPENDENCIES_PATH "$ENV{jdependencies_home}" CACHE PATH "Dependencies location")
 
-	if(DEPENDENCIES_PATH STREQUAL "")
-		message(FATAL_ERROR "Invalid dependencies path")
-	endif()
+if(DEPENDENCIES_PATH STREQUAL "")
+	message(FATAL_ERROR "Invalid dependencies path")
+endif()
 
-	set(DEPENDENCIES_LIBS "${DEPENDENCIES_PATH}/${LIB_DIRECTORY}")
+set(DEPENDENCIES_LIBS "${DEPENDENCIES_PATH}/${LIB_DIRECTORY}")
 
-	add_external_project(${DEPENDENCIES_PATH} FALSE)
-else()
-	find_package(SDL2 REQUIRED)
+add_external_project(${DEPENDENCIES_PATH} FALSE)
 
+find_package(SDL2 QUIET)
+if(SDL2_FOUND)
 	include_directories(${SDL2_INCLUDE_DIRS})
 endif()
