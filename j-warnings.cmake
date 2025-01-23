1245c0ed0de3529c4df9646d37cfd68306cb3f59
Jack Powell <jack9267@users.noreply.github.com>
2025-01-23 02:03:37 +0000

    Don't disable level 4 warnings

--- j-warnings.cmake
+++ j-warnings.cmake
@@ -9,44 +9,11 @@ if(MSVC AND CUSTOM_MSVC_WARNING_LEVEL)
 endif()
 
 if(MSVC)
-	# nonstandard extension used : nameless struct/union
-	disable_warning(4201)
-
-	# 'identifier' : unreferenced formal parameter
-	disable_warning(4100)
-
-	# conditional expression is constant
-	disable_warning(4127)
-
-	# nonstandard extension used : class rvalue used as lvalue
-	disable_warning(4238)
-
-	# Potentially uninitialized local variable 'name' used
-	disable_warning(4701)
-
-	# Potentially uninitialized local pointer variable 'name' used
-	disable_warning(4703)
-
-	# 'identifier' : local variable is initialized but not referenced
-	disable_warning(4189)
-
-	# cast truncates constant value
-	disable_warning(4310)
-
-	# interaction between 'function' and C++ object destruction is non-portable
-	disable_warning(4611)
-
 	# Inline asm assigning to 'FS:0' : handler not registered as safe handler
 	disable_warning(4733)
 
 	# identifier' : class 'type' needs to have dll-interface to be used by clients of class 'type2'
 	disable_warning(4251)
-
-	# structure was padded due to alignment specifier
-	disable_warning(4324)
-
-	# 'conversion' : conversion from 'type1' to 'type2', signed/unsigned mismatch
-	disable_warning(4245)
 
 	# export 'exportname' specified multiple times; using first specification
 	disable_linker_warning(4197)
